14d03bbb4d14d0bc20ec99bde29a1e2ff4cf6b39
Liliia <lilia_halwbvr@icloud.com>
2025-05-29 01:54:11 +0200

    [LIL-26] feature: add edit button

--- src/components/MainSection/Assignment.vue
+++ src/components/MainSection/Assignment.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 const props = defineProps<{
   assignment: { id: number; name: string; description: string; complete: boolean }
 }>()
@@ -6,15 +7,32 @@ const props = defineProps<{
 const emit = defineEmits<{
   (e: 'delete', id: number): void
 }>()
+
+const isEditing = ref(false)
+const editedName = ref(props.assignment.name)
+const editedDescription = ref(props.assignment.description)
+
+function saveEdits(){
+  props.assignment.name = editedName.value
+  props.assignment.description = editedDescription.value
+  isEditing.value = false
+}
 </script>
 
 <template>
   <li class="assignment">
-    <div class="text">
+    <div class="text" v-if="!isEditing">
       <span :class="{ done: assignment.complete }">{{ assignment.name }}</span>
       <p v-if="assignment.description" class="description" :class="{ done: assignment.complete }">{{ assignment.description }}</p>
     </div>
+
+    <div v-else class="text">
+      <input v-model="editedName" >
+      <input v-model="editedDescription" >
+    </div>
     <input type="checkbox" v-model="assignment.complete" />
+    <button v-if="!isEditing" @click="isEditing = true">✏️</button>
+    <button v-else @click="saveEdits">✅</button>
     <button @click="$emit('delete', assignment.id)">❌</button>
   </li>
 </template>
@@ -32,6 +50,20 @@ const emit = defineEmits<{
   flex-direction: column;
   flex-grow: 1;
   font-weight: 700;
+}
+
+.text input {
+  flex-grow: 1;
+  padding: 0.5rem;
+  border-radius: 8px;
+  border: none;
+  font-size: 1rem;
+  color: gray;
+  margin-bottom: 0.5rem;
+}
+
+.text input:last-child{
+  margin-bottom: 0;
 }
 
 .description {
@@ -56,7 +88,7 @@ input[type='checkbox'] {
 button {
   background: transparent;
   border: none;
-  width: 15px;
+  height: 15px;
   cursor: pointer;
   padding: 0;
 }
